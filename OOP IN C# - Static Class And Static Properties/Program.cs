286a0a2607ceefdd18fba21ba6680c163ae800cc
Salah_Eddine_Saadani <salaheddinesaadani@gmail.com>
2025-08-31 15:02:29 +0100

    Add files via upload

---
+++ OOP IN C# - Static Class And Static Properties/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace OOP_IN_C____Static_Class_And_Static_Properties
+{
+    static class Settings
+    {
+        public static int DayNumber
+        {
+            get
+            {
+                return DateTime.Today.Day;
+            }
+        }
+
+        public static string DayName
+        {
+            get
+            {
+                return DateTime.Today.DayOfWeek.ToString();
+            }
+        }
+
+        public static string ProjectPath
+        {
+            get;
+            set;
+        }
+    }
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            // Read the static properties.
+            //
+            Console.WriteLine(Settings.DayNumber);
+            Console.WriteLine(Settings.DayName);
+            //
+            // Change the value of the static bool property.
+            //
+            Settings.ProjectPath = @"C:\MyProjects\";
+            Console.WriteLine(Settings.ProjectPath);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ OOP IN C# - Static Constructor/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace OOP_IN_C____Static_Constructor
+{
+    static class Settings
+    {
+        public static int DayNumber
+        {
+            get
+            {
+                return DateTime.Today.Day;
+            }
+        }
+
+        public static string DayName
+        {
+            get
+            {
+                return DateTime.Today.DayOfWeek.ToString();
+            }
+        }
+
+
+        public static string ProjectPath
+        {
+            get;
+            set;
+        }
+        //this is a static constructor will be called once during the program
+        static Settings()
+        {
+            ProjectPath = @"C:\MyProjects\";
+        }
+    }
+
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            // You cannot create an object here because class is static
+            // Settings Obj1 = new Settings();
+
+            //
+            // Read the static properties.
+            //
+            Console.WriteLine(Settings.DayNumber);
+            Console.WriteLine(Settings.DayName);
+            //
+            // Change the value of the static bool property.
+            //
+
+            Console.WriteLine(Settings.ProjectPath);
+
+            Console.ReadKey();
+        }
+    }
+}
