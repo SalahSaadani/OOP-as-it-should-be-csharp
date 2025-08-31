7eb8dd11a1dbc1f64f9bff06fe37524fc0ba2f1d
Salah_Eddine_Saadani <salaheddinesaadani@gmail.com>
2025-08-31 14:53:53 +0100

    Add files via upload

---
+++ OOP IN C# - Method Overriding in C# Inheritance/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_IN_C____Method_Overriding_in_C__Inheritance
+{
+    public class clsA
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("Hi, I'm the print method from the base class A");
+        }
+    }
+    public class clsB : clsA
+    {
+        public override void Print()
+
+        {
+            Console.WriteLine("Hi, I'm the print method from the derived class B");
+
+            base.Print();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an object of Empoyee
+            clsB ObjB = new clsB();
+
+            ObjB.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
